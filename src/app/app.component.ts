bd772b839ed191f4b89ddd761d4bcdec8d7a763c
Rafael <rafaelteixeiravl@gmail.com>
2024-09-30 13:15:08 -0300

    feat: começo-dos-ajustes-para-o-inicio-do-projeto

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -9,5 +9,9 @@ import { RouterOutlet } from '@angular/router';
   styleUrl: './app.component.css'
 })
 export class AppComponent {
-  title = 'user-crud';
+  title = 'Bem vindo ao curso de FrontEnd com Angular';
+}
+
+export class Form {
+  title = 'Bem vindo ao curso de FrontEnd com Angular';
 }
